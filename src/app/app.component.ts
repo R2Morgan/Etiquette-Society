a6d17d42bd950ebfcc9e5eda2f01a6c65e59b772
r2morgan <stefandobrescu2008@yahoo.com>
2026-02-23 18:55:15 +0200

    v. 0.1 - Initial Alpha Version
    - configure project
    - add basic markdown
    - add initial home page
    - add initial header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {HeaderComponent} from "./components/shared/header/header.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomePageComponent, HeaderComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'EtiquetteSociety';
---
+++ src/assets/members/Flaviu_Celsie/member.md
@@ -0,0 +1,5 @@
+---
+name: "Flaviu Celsie"
+mainImage: "main.jpg"
+images:
+---
