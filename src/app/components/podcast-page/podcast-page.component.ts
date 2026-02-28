08ff41a590b45ea04d4bab9ed49b305d2f776122
r2morgan <stefandobrescu2008@yahoo.com>
2026-02-28 22:40:07 +0200

    v. 0.4.0 - Add Podcast Page
    - add podcast page
    - add link to homepage in header image

--- src/app/components/podcast-page/podcast-page.component.ts
+++ src/app/components/podcast-page/podcast-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ScrollAnimateDirective} from "../../utils/scroll-animate.directive";
 
 @Component({
   selector: 'podcast-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ScrollAnimateDirective
+  ],
   templateUrl: './podcast-page.component.html',
   styleUrl: './podcast-page.component.scss'
 })
