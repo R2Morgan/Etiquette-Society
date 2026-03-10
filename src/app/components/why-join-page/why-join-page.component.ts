5fcd5e743d8289738ef386fda79240b4e3186b95
r2morgan <stefandobrescu2008@yahoo.com>
2026-03-10 23:39:52 +0200

    v. 0.7.0 - Add Why Join Page
    - add Why Join page

--- src/app/components/why-join-page/why-join-page.component.ts
+++ src/app/components/why-join-page/why-join-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {Router} from "@angular/router";
+import {ScrollAnimateDirective} from "../../utils/scroll-animate.directive";
 
 @Component({
   selector: 'why-join-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ScrollAnimateDirective
+  ],
   templateUrl: './why-join-page.component.html',
   styleUrl: './why-join-page.component.scss'
 })
 export class WhyJoinPageComponent {
+  constructor(private router: Router) { }
 
+  goToMemberships() {
+    this.router.navigate(["/memberships"]);
+  }
 }
